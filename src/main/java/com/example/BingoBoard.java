317cf2079223fca25c09cb3c223934d09a59ce0d
haedanui <rhs9705kr@naver.com>
2023-09-24 15:39:15 +0900

    init board() 되게 수정

--- src/main/java/com/example/BingoBoard.java
+++ src/main/java/com/example/BingoBoard.java
@@ -1,8 +1,8 @@
 package com.example;
 
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Set;
-
+import java.util.List;
 public class BingoBoard {
     private static final StringBuilder SB = new StringBuilder();
     private static int boardsize; //static 을 선언하지 않으면 문제가 생기려나?(생길시 수정 바람)
@@ -18,7 +18,8 @@ public class BingoBoard {
 
     //
     private void initBoard() {
-        Set<Integer> set = new HashSet<>(boardsize * boardsize);
+        HashSet<Integer> set = new HashSet<>(boardsize * boardsize);
+        List<Integer> list = new ArrayList<>();
         //TODO size를 받아서 적당한 범위의 값을 책정해 줄 것!
         int range = boardsize * boardsize;//범위 범위를 size*size를 하면 범위가 정해진다.
         int rand;//랜덤값을 임시로 저장하는 곳.
@@ -28,13 +29,18 @@ public class BingoBoard {
                 // 값이 중복이면 아닌 값을 뽑을때 까지 반복
             }
             set.add(rand);
+            list.add(rand);
         }
 
         int index = 0;
-        for (int n: set){
+        for (int n: list){
             board[index/boardsize][index%boardsize] = String.valueOf(n);
             index++;
         }
+        // Iterator iterator = set.iterator();
+        // for(int i = 0; i < range; i++){
+        //     board[i/boardsize][i%boardsize] = String.valueOf(iterator.next());
+        // }
     }
 
     //TODO 빙고의 여부확인을 체크하는 함수 추가
@@ -84,16 +90,16 @@ public class BingoBoard {
                 return true;
             }
         }
-
-
         return false;
     }
+    
     @Override
     public String toString() {
         SB.setLength(0); // init.
+        int range = boardsize * boardsize;
 
-        for (int i = 0; i < boardsize * boardsize; ++i) {
-            SB.append(String.format("[%2d]", board[i / boardsize][i % boardsize]));
+        for (int i = 0; i < range ; ++i) {
+            SB.append(String.format("%5s", Integer.parseInt(board[i / boardsize][i % boardsize])));
             if ((i + 1) % boardsize == 0)
                 SB.append('\n');
         }
@@ -102,7 +108,9 @@ public class BingoBoard {
     }
 
     public static void main(String[] args) {
-            System.out.println(new BingoBoard(5).toString());
+        //System.out.println(new BingoBoard(5).toString());
+        BingoBoard bingoBoard = new BingoBoard(5);//빙고 선언하고 빙고 초기화까지
+        System.out.println(bingoBoard.toString());
     }
 }
 
